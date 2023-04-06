98661efcd61b33fe14c6bab2569136d8e5a253d8
Voutsinos Kostas <voutsinosakostas@gmail.com>
2023-04-06 16:28:05 +0300

    Added CRUD with MediatR and cleaned Program.cs with an extension method for Services

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -20,5 +20,33 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query { Id = id});
         }
+
+        [HttpPost]  // api/activities
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity}));
+        }
+
+        //q: with who does this activity communicate
+        //a: with the mediator
+        //q: what does the mediator do next    
+        //a: it sends the command to the handler
+        //q: what does the handler do next 
+        //a: it saves the activity to the database
+        //q: why do I need the mediator and the handler
+        //a: because we want to keep our controllers clean and we want to keep our controllers thin
+
+        [HttpPut("{id}")]   // api/activities/Guid
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]    // api/activities/Guid
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id}));
+        }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,35 +1,13 @@
-using Application.Activities;
-using MediatR;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Cors.Infrastructure;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    }); 
-});
-
-builder.Services.AddMediatR(typeof(List.Handler));
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
